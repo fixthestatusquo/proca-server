63a9cbabce5a7efa3894513e44324e059984d877
xavier.dutoit <xavier@tttp.eu>
2020-04-05 15:50:03 +0200

    update switch to base64 encoding

--- sdk/cli/index.js
+++ sdk/cli/index.js
@@ -1,10 +1,10 @@
 const config = require('./config/get.js');
 const {getCount,getSignature} = require ('./lib/server.js');
-
 async function start (actionPage) {
+  console.log(config);
   const count = await getCount(actionPage);
   console.log ("actionPage count:" +count);
-  const data= await getSignature();
+  const data= await getSignature({authorization:config.authorization});
   console.log(data);
 }
 
