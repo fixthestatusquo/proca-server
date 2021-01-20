fcdb74bc464962df096e013620326cd2d1f879e0
Marcin Koziej <marcin@cahoots.pl>
2021-01-20 17:34:11 +0100

    Export auth primitives

--- sdk/api/src/index.ts
+++ sdk/api/src/index.ts
@@ -1,5 +1,21 @@
 export * as widget from './queries/widget'
 export * as admin from './queries/admin'
-export {basicAuth, tokenAuth, BasicAuth, TokenAuth} from './auth'
-export {link, httpLink, request, subscribe} from './client'
 export * as types from './types'
+
+// Authentication primitives
+export {
+  basicAuth,
+  tokenAuth,
+  BasicAuth,
+  TokenAuth,
+  AuthHeader
+} from './auth'
+
+// Api operation primitives
+export {
+  link,     // advanced link that can do both get and subscribe requests
+  httpLink, // simple link that can do get requests
+  request,  // run get request
+  subscribe, // run subscribe request
+  ExecutionErrors
+} from './client'
