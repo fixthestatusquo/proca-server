06e62201cf555caed9da2eee1e571cf11dc3ba08
Marcin Koziej <marcin@cahoots.pl>
2021-05-30 11:24:54 +0200

    Bump mix.exs to match next release

--- proca/mix.exs
+++ proca/mix.exs
@@ -4,7 +4,7 @@ defmodule Proca.MixProject do
   def project do
     [
       app: :proca,
-      version: "0.2.0",
+      version: "2.4.0-alpha.1",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
