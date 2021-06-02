ee9a17f729782bf6206c57e415086d0138c77148
Marcin Koziej <marcin@cahoots.pl>
2021-06-02 16:17:29 +0200

    Fix types related to confirm changes

--- sdk/api/src/types.ts
+++ sdk/api/src/types.ts
@@ -8,7 +8,12 @@ export type ActionPageRecord = Omit<types.ActionPage, 'org' | 'campaign'>;
 export type CampaignRecord = Omit<types.Campaign, 'org' | 'stats' | 'actions' | 'partnerships'>;
 // campaign record with owning org relation, useful because campaigns are visible to all orgs
 export type CampaignOrg = Omit<types.Campaign, 'stats' | 'actions' | 'partnerships'>;
-export type OrgRecord = Omit<types.Org, 'services' | 'key' | 'keys' | 'campaign' | 'campaigns' | 'actionPages' | 'actionPage'>;
+export type OrgRecord = Omit<types.Org, 'services' | 'key' | 'keys' | 'campaign' | 'campaigns' | 'actionPages' | 'actionPage' | 'users'>;
 export type CampaignIds = types.ActionCampaign;
 
+export type PartnershipRecord = {
+  org: types.PublicOrg;
+  actionPages: Array<ActionPageRecord>;
+  launchRequests: Array<types.Confirm>;
+};
 
