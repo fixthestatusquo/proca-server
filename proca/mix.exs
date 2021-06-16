90d2aa7aafd320b70b6eb1c273159cab1ca22a8b
Marcin Koziej <marcin@cahoots.pl>
2021-06-16 16:37:24 +0200

    Donation api stabilized

--- proca/mix.exs
+++ proca/mix.exs
@@ -4,7 +4,7 @@ defmodule Proca.MixProject do
   def project do
     [
       app: :proca,
-      version: "2.4.0-alpha.1",
+      version: "2.4.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
