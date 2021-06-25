353e6689adf2b04ed702b23682ab8a597684b9b1
Marcin Koziej <marcin@cahoots.pl>
2021-06-25 18:14:30 +0200

    Bump version to 2.4.1 (only backend)

--- proca/mix.exs
+++ proca/mix.exs
@@ -4,7 +4,7 @@ defmodule Proca.MixProject do
   def project do
     [
       app: :proca,
-      version: "2.4.0",
+      version: "2.4.1",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
