f1e43a2197c675b4bc20092cfe995ce27e217522
Marcin Koziej <marcin@cahoots.pl>
2021-08-07 10:58:31 +0200

    Add a missing file!

---
+++ proca/lib/proca/action_page/status.ex
@@ -0,0 +1,92 @@
+defmodule Proca.ActionPage.Status do 
+  @moduledoc """
+  Server which tracks and monitors active pages.
+
+  Stored maps:
+  - last_seen: id -> last action time and source location
+
+  """
+  use GenServer
+  
+  import Ecto.Query
+  import Proca.Repo, only: [all: 1, one: 1]
+
+  alias Proca.{Action, ActionPage}
+
+  def start_link(_opts) do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  @impl true 
+  def init([]) do 
+    last_seen_at = :ets.new(:last_seen_at, [:set, :protected, :named_table])
+    last_seen_location = :ets.new(:last_seen_location, [:set, :protected, :named_table])
+
+    {:ok, %{
+      last_seen_at: last_seen_at,
+      last_seen_location: last_seen_location
+    }, {:continue, :load_last_seen}}
+  end
+
+  @impl true
+  def handle_continue(:load_last_seen, s) do 
+
+    rows = all from(a in Action, 
+      left_join: s in assoc(a, :source),
+      order_by: [desc: a.id],
+      where: a.inserted_at >= fragment("current_date - interval '1 year'"),
+      select: [a.action_page_id, a.inserted_at, s.location],
+      distinct: [a.action_page_id])
+
+    :ets.insert(:last_seen_at, Enum.map(rows, fn [id, at, _lo] -> {id, at} end))
+    :ets.insert(:last_seen_location, Enum.map(rows, fn [id, _at, lo] -> {id, lo} end))
+
+    {:noreply, s}
+  end
+
+  @impl true
+  def handle_cast({:action, action = %Action{
+    id: id, inserted_at: seen_at
+  }}, s) do 
+
+    :ets.insert(:last_seen_at, {id, seen_at})
+
+    if action.source_id != nil do 
+      source = one Ecto.assoc(action, [:source])
+      if source.location do 
+        :ets.insert(:last_seen_location, {id, action.source.location})
+      end
+    end
+
+    {:noreply, s}
+  end
+
+  def track_action(action = %Action{}) do 
+    GenServer.cast(__MODULE__, {:action, action})
+
+  end
+  
+  @spec activity(number) :: :inactive | {:active, NaiveDateTime, String.t | nil}
+  def activity(action_page_id) when is_number(action_page_id) do 
+    case get_last_at(action_page_id) do 
+      nil -> :inactive
+      seen_at -> {:active, seen_at, get_last_location(action_page_id)}
+    end 
+  end
+
+  def get_last_at(id) do 
+    case :ets.lookup(:last_seen_at, id) do 
+      [] -> nil 
+      [{_id, seen_at}] -> seen_at
+    end
+  end
+
+  def get_last_location(id) do 
+    case :ets.lookup(:last_seen_location, id) do 
+      [] -> nil 
+      [{_id, location}] -> location
+    end
+  end
+
+
+end
