2dbb24ae05c2680e165048b99ab8498534d8e5e6
Marcin Koziej <marcin@cahoots.pl>
2021-09-15 00:06:30 +0200

    Check JWT token content

--- proca/lib/proca_web/plugs/jwt_auth_plug.ex
+++ proca/lib/proca_web/plugs/jwt_auth_plug.ex
@@ -27,6 +27,7 @@ defmodule ProcaWeb.Plugs.JwtAuthPlug do
   def jwt_auth(conn, param) do
     with token when not is_nil(token) <- get_token(conn, param),
          {true, jwt, _sig} <- Proca.Server.Jwks.verify(token),
+         true <- check_has_session(jwt),
          :ok <- check_email_verified(jwt)
      do
       conn
@@ -38,27 +39,44 @@ defmodule ProcaWeb.Plugs.JwtAuthPlug do
       :unverified -> 
         error_halt(conn, 401, "unauthorized", "Email not verified")
 
+      # token with no identity traits
+      false -> 
+        error_halt(conn, 401, "unauthorized", "JWT token has invalid data")
+
       # no token
       nil ->
         conn
     end
   end
 
+  def check_has_session(%JOSE.JWT{
+    fields: %{
+      "session" => %{
+        "identity" => %{
+          "traits" => %{"email" => _email},
+          "verifiable_addresses" => _emails
+        }
+      }
+    }
+  }), do: true 
+
+  def check_has_session(_), do: false
+
   def check_email_verified(jwt) do 
     if need_verified_email?() do
-      %JOSE.JWT{
-        fields: %{
-          "session" => %{
-            "identity" => %{
-              "traits" => %{"email" => email},
-              "verifiable_addresses" => emails
+      current = case jwt do 
+        %JOSE.JWT{
+          fields: %{
+            "session" => %{
+              "identity" => %{
+                "traits" => %{"email" => email},
+                "verifiable_addresses" => emails
+              }
             }
           }
-        }
-      } = jwt
-
-      current = Enum.find(emails, 
-        fn %{"value" => v} -> v == email end) 
+        } ->  Enum.find(emails, fn %{"value" => v} -> v == email end) 
+        _ -> %{}
+      end
 
       if current["verified"] do 
         :ok 
