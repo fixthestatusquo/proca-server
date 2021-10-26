9773d9767a0a7a1bb97d3bfed0bc31a8b96f1557
Marcin Koziej <marcin@cahoots.pl>
2021-10-26 22:40:27 +0200

    Fix tests

--- proca/test/proca_web/resolvers/org_mutatuions_test.exs
+++ proca/test/proca_web/resolvers/org_mutatuions_test.exs
@@ -13,20 +13,23 @@ defmodule ProcaWeb.OrgMutationsTest do
   end
 
   test "Can add an org", %{user: user, org_params: p} do
-    {:ok, o} = ProcaWeb.Resolvers.Org.add_org(0, %{input: p}, %{context: %{user: user}})
+    r = ProcaWeb.Resolvers.Org.add_org(0, %{input: p}, %{context: %{auth: %Proca.Auth{user: user}}})
+    assert {:middleware, ProcaWeb.Resolvers.ChangeAuth, {auth, {:ok, o}}} = r
     %{name: name} = o
     assert name == p[:name]
+    assert not is_nil auth.staffer
+    assert auth.staffer.org.name == p[:name]
   end
 
   test "Can't add an org with incorrect params", %{user: user, org_params: p} do
     bad_name = %{p | name: "test!"}
-    er = ProcaWeb.Resolvers.Org.add_org(0, %{input: bad_name}, %{context: %{user: user}})
-    assert {:error, [%{message: "name: has invalid format", path: ["name"]}]} = er
+    er = ProcaWeb.Resolvers.Org.add_org(0, %{input: bad_name}, %{context: %{auth: %Proca.Auth{user: user}}})
+    assert {:error, %Ecto.Changeset{errors: [{:name, {"has invalid format", [validation: :format]}} | _ ]}} = er
 
     no_title = Map.delete(p, :title)
 
-    er = ProcaWeb.Resolvers.Org.add_org(0, %{input: no_title}, %{context: %{user: user}})
-    assert {:error, [%{message: "title: can't be blank", path: ["title"]}]} = er
+    er = ProcaWeb.Resolvers.Org.add_org(0, %{input: no_title}, %{context: %{auth: %Proca.Auth{user: user}}})
+    assert {:error, %Ecto.Changeset{errors: [{:title, {"can't be blank", _}} | _ ]}} = er
 
   end
 
