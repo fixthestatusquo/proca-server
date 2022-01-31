6c2a5a47fe82568584ff9ad62764a1f6de3fa0be
Marcin Koziej <marcin@cahoots.pl>
2022-01-31 23:36:11 +0100

    Add onlyDoubleOptIn filter to exportActions()

--- proca/lib/proca_web/resolvers/export_actions.ex
+++ proca/lib/proca_web/resolvers/export_actions.ex
@@ -35,16 +35,19 @@ defmodule ProcaWeb.Resolvers.ExportActions do
 
   def filter_campaign(q, _), do: q
 
-  def filter_optin(q, %{only_opt_in: false}) do
-    q
-  end
+  @doc "Default to opt in only"
+  def filter_optin(q, %{only_opt_in: false}), do: q
 
   def filter_optin(q, _) do
-    # on only_opt_in=true or omitted
-    # filter by communication consent
-    q
-    |> where([a, s, c], c.communication_consent == true)
+    where(q, [a, s, c], c.communication_consent == true)
   end
+
+  @doc "Default to ignore doi"
+  def filter_doubleoptin(q, %{only_double_opt_in: true}) do
+    where(q, [a, s, c], s.email_status == :double_opt_in)
+  end
+
+  def filter_doubleoptin(q, _), do: q
 
   def format_contact(
         %Supporter{fingerprint: ref},
@@ -140,6 +143,7 @@ defmodule ProcaWeb.Resolvers.ExportActions do
     |> filter_after(params)
     |> filter_campaign(params)
     |> filter_optin(params)
+    |> filter_doubleoptin(params)
     |> order_by([a], asc: a.id)
     |> Repo.all()
     |> Enum.map(&format/1)
