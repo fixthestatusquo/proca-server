2076bd1dc66d1c8e4376d2360ccf2e66820dba3c
Marcin Koziej <marcin@cahoots.pl>
2022-04-05 10:22:02 +0200

    Fix validations for phone, country, postcode

--- proca/test/contact_data_test.exs
+++ proca/test/contact_data_test.exs
@@ -19,4 +19,18 @@ defmodule ContactDataTest do
 
     assert %BasicData{first_name: "Harald", last_name: "Bower", email: "harhar@gmail.com"} = data
   end
+
+  test "Phone must have a good format" do
+    params = %{
+      first_name: "Caller",
+      email: "caller@somewhere.org"
+    }
+
+    new_data = BasicData.from_input(params |> Map.put(:phone, "Street 123"))
+    assert %{errors: [phone: {"has invalid format", [validation: :format]}]} = new_data
+
+    new_data = BasicData.from_input(params |> Map.put(:phone, "+48123456789"))
+    data = apply_changes(new_data)
+    assert data = %{phone: "+48123456789"}
+  end
 end
