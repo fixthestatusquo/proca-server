e54a74cb9182d7c15822eb16fab8d5bce0e690de
Marcin Koziej <marcin@cahoots.pl>
2022-04-05 12:37:05 +0200

    Add optional address key to basic data schema
    It will hold a specific address:
    region, locality, street,  street_number - only if one of them is given.
    otherwise null

--- proca/test/contact_data_test.exs
+++ proca/test/contact_data_test.exs
@@ -17,7 +17,12 @@ defmodule ContactDataTest do
     new_data = BasicData.from_input(params)
     data = apply_changes(new_data)
 
-    assert %BasicData{first_name: "Harald", last_name: "Bower", email: "harhar@gmail.com"} = data
+    assert %BasicData{
+             first_name: "Harald",
+             last_name: "Bower",
+             email: "harhar@gmail.com",
+             address: nil
+           } = data
   end
 
   test "Phone must have a good format" do
@@ -35,5 +40,34 @@ defmodule ContactDataTest do
     new_data = BasicData.from_input(params |> Map.put(:phone, "+48123456789"))
     data = apply_changes(new_data)
     assert data = %{phone: "+48123456789"}
+  end
+
+  test "If some address is provided, add it to payload" do
+    params = %{
+      first_name: "Harald",
+      last_name: "Bower",
+      email: "harhar@gmail.com",
+      address: %{
+        country: "it",
+        postcode: "0993",
+        locality: "Warszawa",
+        street: "Kwiatowa",
+        street_number: "12A"
+      }
+    }
+
+    new_data = BasicData.from_input(params)
+    data = apply_changes(new_data)
+
+    assert %BasicData{
+             first_name: "Harald",
+             last_name: "Bower",
+             email: "harhar@gmail.com",
+             address: %{
+               locality: "Warszawa",
+               street: "Kwiatowa",
+               street_number: "12A"
+             }
+           } = data
   end
 end
