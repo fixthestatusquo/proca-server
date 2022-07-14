ea7c414710fbd0d44944b93239f530d6218f6b0f
Marcin Koziej <marcin@cahoots.pl>
2022-07-14 13:21:03 +0200

    update dupe_rank migration to contain SQL for old actions

--- proca/priv/repo/migrations/20220603104555_add_dupe_rank_to_supporters.exs
+++ proca/priv/repo/migrations/20220603104555_add_dupe_rank_to_supporters.exs
@@ -5,5 +5,20 @@ defmodule Proca.Repo.Migrations.AddDupeRankToSupporters do
     alter table(:supporters) do
       add :dupe_rank, :integer, null: true
     end
+
+    execute """
+            UPDATE supporters
+            SET dupe_rank = r.rank -1
+            FROM
+            (
+              SELECT
+                s.id,
+                rank() over (partition by s.campaign_id, s.fingerprint order by s.inserted_at)
+              FROM supporters s
+            ) r
+            WHERE
+            supporters.id = r.id
+            """,
+            ""
   end
 end
