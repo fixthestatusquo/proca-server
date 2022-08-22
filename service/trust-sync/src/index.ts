3420533bd7b5c56dff0d00255f5c7e0600e76b50
1v4n4 <ivananl@yahoo.com>
2022-08-22 18:40:12 +0200

    Add trust-sync service

---
+++ service/trust-sync/src/index.ts
@@ -0,0 +1,66 @@
+const crypto = require('crypto');
+const axios = require('axios').default
+const dotenv = require('dotenv');
+dotenv.config();
+
+// {
+//   protected $_secret;
+//   protected $_ttl;
+//   protected $_digest;
+
+//   public function __construct($secret, $ttl=30, $digest='sha256')
+//   {
+//     $this->_secret = $secret;
+//     $this->_ttl = $ttl;
+//     $this->_digest = $digest;
+//   }
+
+//   public function generate()
+//   {
+//     return hash_hmac($this->_digest, $this->stamp(), $this->_secret);
+//   }
+
+//   protected function stamp()
+//   {
+//    return (string) ((int) (time() / $this->_ttl));
+//   }
+// }
+
+// base64_encode(hash_hmac('sha256', $stamp, $key));
+
+
+const key = process.env['TRUST_KEY'];
+console.log("key", key)
+const stamp = (Math.round(Date.now() / 1000)/30).toString();
+
+let token = crypto.createHmac("sha256", key).update(stamp).digest().toString('base64');
+
+const headers = {
+  headers: {
+    'Authorization': `Token token=proca-test:"${token}"`
+  }
+}
+const url = "https://lc-trust-stage.palasthotel.de/api/v1/ping"
+
+async function getPing() {
+  try {
+    const { data, status } = await axios.post(
+      url,
+      headers,
+    );
+
+    console.log(data);
+    console.log('Status: ', status);
+    return data;
+  } catch (error: any) {
+    if (axios.isAxiosError(error)) {
+      console.log('error: ', error.code, error.config.data, error.message);
+      return error;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
+
+getPing();
