4724e354ec5d880ea82506b8f1e992c8f2f35829
1v4n4 <ivananl@yahoo.com>
2022-08-23 15:20:28 +0200

    Add postAction feature

---
+++ service/trust-sync/src/data.ts
@@ -0,0 +1,46 @@
+
+import type { ActionMessageV2, EventMessageV2 } from '@proca/queue'
+const _ = require("lodash");
+
+export interface TrustAction {
+  first_name: string;
+  last_name?: string | null;
+  address?: string | null;
+  zip_code?: string | null;
+  location?: string | null;
+  email: string;
+  phone?: string | null;
+  country?: string | null;
+  message?: string | null;
+  subscribe_newsletter: boolean | undefined;
+  move_code: string;
+  origin: string;
+}
+
+export interface Signature {
+  "petition_signature": TrustAction;
+}
+
+export const formatAction = (queueAction : ActionMessageV2) => {
+  let action: TrustAction = {
+    first_name: queueAction.contact.firstName,
+    last_name: queueAction.contact.lastName,
+    address: queueAction.contact.adress,
+    zip_code: queueAction.contact.postcode,
+    location: queueAction.contact.city
+      || queueAction.contact.area
+      || queueAction.contact.locality
+      || queueAction.contact.region,
+    email: queueAction.contact.email,
+    phone: queueAction.contact.phone,
+    country: queueAction.contact.country,
+    message: queueAction.contact.comment,
+    subscribe_newsletter: queueAction.privacy.optIn,
+    move_code: queueAction.actionPage.name,
+    origin: queueAction.tracking.location
+  }
+
+  const signature: Signature = { "petition_signature": _.omitBy(action, _.isNil) };
+
+  return signature;
+  }
--- service/trust-sync/src/index.ts
+++ service/trust-sync/src/index.ts
@@ -1,69 +1,19 @@
-const crypto = require('crypto');
-const axios = require('axios').default
+import { syncQueue, ActionMessageV2, EventMessageV2 } from '@proca/queue';
+import { Signature, formatAction } from "./data";
+import { postAction } from "./client";
 const dotenv = require('dotenv');
 dotenv.config();
 
-// {
-//   protected $_secret;
-//   protected $_ttl;
-//   protected $_digest;
-
-//   public function __construct($secret, $ttl=30, $digest='sha256')
-//   {
-//     $this->_secret = $secret;
-//     $this->_ttl = $ttl;
-//     $this->_digest = $digest;
-//   }
-
-//   public function generate()
-//   {
-//     return hash_hmac($this->_digest, $this->stamp(), $this->_secret);
-//   }
-
-//   protected function stamp()
-//   {
-//    return (string) ((int) (time() / $this->_ttl));
-//   }
-// }
 
 
+const user = process.env.RABBIT_USER;
+const pass = process.env.RABBIT_PASSWORD;
+const queueDeliver = "cus.172.deliver";
 
-const key = process.env['TRUST_KEY'];
-console.log("key", key)
-const stamp = Math.floor(Math.floor(Date.now() / 1000)/30).toString();
-
-let token = crypto.createHmac("sha256", key).update(stamp).digest().toString('hex');
-
-const headers = {
-  headers: {
-    'Authorization': `Token token="proca-test:${token}"`,
-    'Content-Type': 'application/json'
+syncQueue(`amqps://${user}:${pass}@api.proca.app/proca_live`, queueDeliver, async (action: ActionMessageV2 | EventMessageV2) => {
+  if (action.schema === 'proca:action:2') {
+    console.log("ooooooo", formatAction(action))
+    await postAction(formatAction(action));
   }
-}
-const url = "https://lc-trust-stage.palasthotel.de/api/v1/ping"
-
-async function getPing() {
-  try {
-    const { data, status } = await axios.post(
-      url,
-      {},
-      headers,
-    );
-
-    console.log(JSON.stringify(data));
-    console.log('Status: ', status);
-    return data;
-  } catch (error: any) {
-    if (axios.isAxiosError(error)) {
-      console.log('error: ', error.code, error.config.data, error.message, error.response.status, error.response.statusText);
-      return error.message;
-    } else {
-      console.log('unexpected error: ', error);
-      return 'An unexpected error occurred';
-    }
-  }
-}
-
-console.log(crypto.createHmac('sha256', 'secret').update('data').digest().toString('hex'))
-
-getPing();
+}, {}
+)
