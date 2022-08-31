00c5d7a68296f004bd51416df0372e4ec74ccaa1
1v4n4 <ivananl@yahoo.com>
2022-08-31 17:15:08 +0200

    Save action  from confirm queue to leveldb as action and retry record

--- service/confirmation-repeater/src/index.ts
+++ service/confirmation-repeater/src/index.ts
@@ -11,6 +11,39 @@ const queueConfirm = process.env.RABBIT_QUEUE || "";
 
 syncQueue(`amqps://${user}:${pass}@api.proca.app/proca_live`, queueConfirm, async (action: ActionMessageV2 | EventMessageV2) => {
   if (action.schema === 'proca:action:2') {
-    console.log("action", action);
-  }
+
+    console.log(action.actionId);
+    await db.get('action' + action.actionId, async function (error: any, value: any) {
+      if (error?.status === 404 || !value?.done) {
+
+        await db.put('action' + action.actionId, action, async function (error: any) {
+          if (error) {
+            throw error
+          }
+          db.get('action' + action.actionId, function (error: any, value: any) {
+            if (error) throw error;
+            console.log("saved action:", value)
+
+          })
+        })
+
+        const created = new Date(action.action.createdAt)
+        const retry = { retry: (new Date(created.setDate(created.getDate() + 3))).toISOString(), attempts: 0 };
+
+        await db.put('retry' + action.actionId, retry, async function (error: any) {
+          if (error) {
+            throw error
+          }
+          db.get('retry' + action.actionId, function (error: any, value: any) {
+            if (error) throw error;
+            console.log("saved retry:", value)
+
+          })
+        })
+      }
     })
+
+      }
+    })
+
+
