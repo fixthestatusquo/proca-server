44282b6c1608ad01dd4baaaa994309d999a3ebe1
1v4n4 <ivananl@yahoo.com>
2022-09-01 09:32:45 +0200

    Add job for checking retry dates

--- service/confirmation-repeater/src/index.ts
+++ service/confirmation-repeater/src/index.ts
@@ -1,26 +1,40 @@
 import { syncQueue, ActionMessageV2, EventMessageV2 } from '@proca/queue';
 const { Level } = require("level");
-
+const schedule = require('node-schedule');
+//const nodeSchedule = require("@types/node-schedule")
 const db = new Level(process.env.DB_PATH || "./db", { valueEncoding: 'json' })
+
 const dotenv = require('dotenv');
 dotenv.config();
 
 const user = process.env.RABBIT_USER;
 const pass = process.env.RABBIT_PASSWORD;
-const queueConfirm = process.env.RABBIT_QUEUE || "";
+const queueConfirm = process.env.CONFIRM_QUEUE || "";
+
+const job = schedule.scheduleJob('* * * * *', async () => {
+  console.log('running every hour');
+  for await (const [key, value] of db.iterator({ gt: 'retry-' })) {
+    const date = new Date(value.retry)
+    console.log(date) // 2
+    if (date > Date.now()) {
+      //reinsert to queue
+    }
+  }
+
+});
 
 syncQueue(`amqps://${user}:${pass}@api.proca.app/proca_live`, queueConfirm, async (action: ActionMessageV2 | EventMessageV2) => {
   if (action.schema === 'proca:action:2') {
 
     console.log(action.actionId);
-    await db.get('action' + action.actionId, async function (error: any, value: any) {
+    await db.get('action-' + action.actionId, async function (error: any, value: any) {
       if (error?.status === 404 || !value?.done) {
 
-        await db.put('action' + action.actionId, action, async function (error: any) {
+        await db.put('action-' + action.actionId, action, async function (error: any) {
           if (error) {
             throw error
           }
-          db.get('action' + action.actionId, function (error: any, value: any) {
+          db.get('action-' + action.actionId, function (error: any, value: any) {
             if (error) throw error;
             console.log("saved action:", value)
 
@@ -30,11 +44,11 @@ syncQueue(`amqps://${user}:${pass}@api.proca.app/proca_live`, queueConfirm, asyn
         const created = new Date(action.action.createdAt)
         const retry = { retry: (new Date(created.setDate(created.getDate() + 3))).toISOString(), attempts: 0 };
 
-        await db.put('retry' + action.actionId, retry, async function (error: any) {
+        await db.put('retry-' + action.actionId, retry, async function (error: any) {
           if (error) {
             throw error
           }
-          db.get('retry' + action.actionId, function (error: any, value: any) {
+          db.get('retry-' + action.actionId, function (error: any, value: any) {
             if (error) throw error;
             console.log("saved retry:", value)
 
@@ -50,7 +64,6 @@ syncQueue(`amqps://${user}:${pass}@api.proca.app/proca_live`, queueConfirm, asyn
 
 // syncQueue(`amqps://${user}:${pass}@api.proca.app/proca_live`, queueDeliveryRepeater, async (action: ActionMessageV2 | EventMessageV2) => {
 //   if (action.schema === 'proca:action:2') {
-
 //     console.log(action.actionId);
 //     await db.put('done' + action.actionId, { done: true }, function (error: any) {
 //       if (error) {
