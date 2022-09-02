aaaa7ba4cd168dc4214161624807d49fcb61d1ee
1v4n4 <ivananl@yahoo.com>
2022-09-02 13:38:09 +0200

    Add customized retry interval array

--- service/confirmation-repeater/src/helpers.ts
+++ service/confirmation-repeater/src/helpers.ts
@@ -1,9 +1,15 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const retryInterval = process.env.RETRY_INTERVAL || '1';
+let retryArray: string[] = [];
+if (process.env.RETRY_INTERVAL) retryArray = process.env.RETRY_INTERVAL.split(",");
 
-export const changeDate = (date: string): string => {
+console.log("r", retryArray);
+
+
+export const changeDate = (date: string, attempts: number): string => {
+  let retryInterval = 2;
+  retryArray[attempts-1] ? retryInterval = +retryArray[attempts-1]: retryInterval;
   const oldDate = new Date(date)
-  return new Date(oldDate.setDate(oldDate.getDate() + +retryInterval)).toISOString();
+  return new Date(oldDate.setDate(oldDate.getDate() + retryInterval)).toISOString();
 };
