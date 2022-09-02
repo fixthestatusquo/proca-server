0c651d8752183690f49da0faae36f9e8f1b40fa5
Marcin Koziej <marcin@cahoots.pl>
2022-09-02 10:53:22 +0200

    IEX colors

--- proca/.iex.exs
+++ proca/.iex.exs
@@ -19,3 +19,15 @@ alias Proca.{Org, Staffer, PublicKey}
 alias Proca.Server.{Encrypt, Notify, Stat}
 import Ecto.Query, only: [from: 2]
 import Ecto.Changeset
+
+IEx.configure(
+  colors: [
+    syntax_colors: [
+      number: :magenta,
+      atom: :cyan,
+      string: :green,
+      boolean: :magenta,
+      nil: :magenta
+    ]
+  ]
+)
