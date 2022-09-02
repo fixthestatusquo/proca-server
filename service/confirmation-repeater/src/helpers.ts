fb4a1a8ee689918ac8a35948425421d775286c71
1v4n4 <ivananl@yahoo.com>
2022-09-02 14:11:12 +0200

    Remove log

--- service/confirmation-repeater/src/helpers.ts
+++ service/confirmation-repeater/src/helpers.ts
@@ -4,9 +4,6 @@ dotenv.config();
 let retryArray: string[] = [];
 if (process.env.RETRY_INTERVAL) retryArray = process.env.RETRY_INTERVAL.split(",");
 
-console.log("r", retryArray);
-
-
 export const changeDate = (date: string, attempts: number): string => {
   let retryInterval = 2;
   retryArray[attempts-1] ? retryInterval = +retryArray[attempts-1]: retryInterval;
