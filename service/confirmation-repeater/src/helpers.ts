e4cc91e400717fc307890febd69b63dc40e30356
1v4n4 <ivananl@yahoo.com>
2022-09-15 11:02:47 +0200

    Replace unnecessary ternary with if, and + with parseInt for clarity

--- service/confirmation-repeater/src/helpers.ts
+++ service/confirmation-repeater/src/helpers.ts
@@ -6,7 +6,9 @@ if (process.env.RETRY_INTERVAL) retryArray = process.env.RETRY_INTERVAL.split(",
 
 export const changeDate = (date: string, attempts: number): string => {
   let retryInterval = 2;
-  retryArray[attempts-1] ? retryInterval = +retryArray[attempts-1]: retryInterval;
+  if (retryArray[attempts - 1]) {
+    retryInterval = parseInt(retryArray[attempts - 1])
+  }
   const oldDate = new Date(date)
   return new Date(oldDate.setDate(oldDate.getDate() + retryInterval)).toISOString();
 };
