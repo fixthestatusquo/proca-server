e0beeaca3d54806e80428c1d129093e125a0b8b7
Marcin Koziej <marcin@cahoots.pl>
2022-09-16 17:35:05 +0200

    Fixes in confirmation repeater

--- service/confirmation-repeater/src/helpers.ts
+++ service/confirmation-repeater/src/helpers.ts
@@ -1,13 +1,15 @@
-const dotenv = require('dotenv');
-dotenv.config();
+let retryArray: number[] = [];
+if (process.env.RETRY_INTERVAL) retryArray = process.env.RETRY_INTERVAL.split(",").map(x => parseInt(x)).filter(x => x > 0);
 
-let retryArray: string[] = [];
-if (process.env.RETRY_INTERVAL) retryArray = process.env.RETRY_INTERVAL.split(",");
-
+/*
+ * Date - datetime in ISO format
+ * attempts - how many previous attempts were there (for first confirmation = 1)
+ * if RETRY_INTERVAL not configured, use interval of 2
+ */
 export const changeDate = (date: string, attempts: number): string => {
   let retryInterval = 2;
   if (retryArray[attempts - 1]) {
-    retryInterval = parseInt(retryArray[attempts - 1])
+    retryInterval = retryArray[attempts - 1]
   }
   const oldDate = new Date(date)
   return new Date(oldDate.setDate(oldDate.getDate() + retryInterval)).toISOString();
