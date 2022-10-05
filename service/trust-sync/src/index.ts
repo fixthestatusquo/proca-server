083de3eca64d5bcde1ef2aa9a91ac72ce0bc2759
1v4n4 <ivananl@yahoo.com>
2022-10-05 17:08:20 +0200

    Fix data_handling_consent in verification payload

--- service/trust-sync/src/index.ts
+++ service/trust-sync/src/index.ts
@@ -1,6 +1,6 @@
 import { syncQueue, ActionMessageV2, EventMessageV2 } from '@proca/queue';
-import { formatAction, Verification } from "./data";
-import { postAction, verification, lookup, rabbit } from "./client";
+import { formatAction, handleConsent } from "./data";
+import { postAction, verification, rabbit } from "./client";
 var argv = require('minimist')(process.argv.slice(2));
 
 const dotenv = require('dotenv');
@@ -13,7 +13,7 @@ const syncer = async () => {
     syncQueue(`amqps://${user}:${pass}@api.proca.app/proca_live`, queueDeliver, async (action: ActionMessageV2 | EventMessageV2) => {
       if (action.schema === 'proca:action:2') {
         const actionPayload = formatAction(action)
-        const verificationPayload = { "petition_signature": { "subscribe_newsletter": actionPayload.petition_signature.subscribe_newsletter, "data_handling_consent": true } }
+        const verificationPayload = { "petition_signature": { "subscribe_newsletter": actionPayload.petition_signature.subscribe_newsletter, "data_handling_consent": handleConsent(action) } };
         const data = await postAction(actionPayload);
         if (data.petition_signature?.verification_token) {
           await verification(data.petition_signature.verification_token, verificationPayload)
