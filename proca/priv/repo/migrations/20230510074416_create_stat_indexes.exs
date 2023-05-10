181e3a74dee423bbc14e0f18b97656d150f13016
Marcin Koziej <marcin@cahoots.pl>
2023-05-10 10:27:48 +0200

    Indexes 2

--- proca/priv/repo/migrations/20230510074416_create_stat_indexes.exs
+++ proca/priv/repo/migrations/20230510074416_create_stat_indexes.exs
@@ -2,8 +2,10 @@ defmodule Proca.Repo.Migrations.CreateStatIndexes do
   use Ecto.Migration
 
   def change do
-    create index(:supporters, [:action_page_id, :campaign_id, :processing_status])
-    create index(:actions, [:processing_status, :action_page_id])
-
+    create index(:supporters, [:action_page_id])
+    create index(:supporters, [:campaign_id])
+    create index(:supporters, [:processing_status])
+    create index(:actions, [:processing_status])
+    create index(:actions, [:action_page_id])
   end
 end
