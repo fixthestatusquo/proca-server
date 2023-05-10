beabfe3309c679a94cd4662cbe29910061591df8
Marcin Koziej <marcin@cahoots.pl>
2023-05-10 10:35:10 +0200

    Index 3

--- proca/priv/repo/migrations/20230510074416_create_stat_indexes.exs
+++ proca/priv/repo/migrations/20230510074416_create_stat_indexes.exs
@@ -3,7 +3,6 @@ defmodule Proca.Repo.Migrations.CreateStatIndexes do
 
   def change do
     create index(:supporters, [:action_page_id])
-    create index(:supporters, [:campaign_id])
     create index(:supporters, [:processing_status])
     create index(:actions, [:processing_status])
     create index(:actions, [:action_page_id])
