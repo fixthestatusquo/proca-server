28057e74acb668329c785b5cefc9500da3403adf
Przemyslaw Pietras <przemyslawp94@gmail.com>
2023-07-25 11:24:37 +0200

    Add table test for checking permissions

---
+++ proca/test/support/fixtures/query_fixtures.ex
@@ -0,0 +1,31 @@
+defmodule Proca.QueryFixtures do
+  def invite_user(%{org_name: org_name}) do
+    """
+    mutation {
+      inviteOrgUser(orgName: "#{org_name}", message: "Welcome to our team", input: {email: "foo@example.com", role: "translator"})  {
+        code email objectId message
+      }
+    }
+    """
+  end
+
+  def add_campaign(%{org_name: org_name}) do
+    """
+    mutation {
+      addCampaign(orgName: "#{org_name}", input: {title: "campaign", name: "campaign"}) {
+        id
+      }
+    }
+    """
+  end
+
+  def update_action_page(%{action_page_id: id}) do
+    """
+    mutation {
+      updateActionPage(id: #{id}, input: {locale: "pl"}) {
+        id
+      }
+    }
+    """
+  end
+end
