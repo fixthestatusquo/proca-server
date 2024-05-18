3253043298690970a9ef559184e785aee9521703
=Xavier <github@tttp.eu>
2024-05-18 09:15:35 +0200

    release 3.4.4, with better removal of targets

--- proca/mix.exs
+++ proca/mix.exs
@@ -4,7 +4,7 @@ defmodule Proca.MixProject do
   def project do
     [
       app: :proca,
-      version: "3.4.3",
+      version: "3.4.4",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
