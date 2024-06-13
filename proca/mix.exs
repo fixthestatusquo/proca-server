5f8f5c544487a9775fa8e6bcbccd879bd0ff009f
Przemyslaw Pietras <przemyslawp94@gmail.com>
2024-06-13 11:18:07 +0200

    Update version to 3.6.0

--- proca/mix.exs
+++ proca/mix.exs
@@ -4,7 +4,7 @@ defmodule Proca.MixProject do
   def project do
     [
       app: :proca,
-      version: "3.5.0",
+      version: "3.6.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
