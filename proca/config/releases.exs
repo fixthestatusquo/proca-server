67787e697b7b81e3272b22fc730037a5831c0bb6
Val Packett <val@packett.cool>
2025-06-03 04:40:30 -0300

    Add env var for prometheus port (#297)

--- proca/config/releases.exs
+++ proca/config/releases.exs
@@ -121,6 +121,10 @@ config :proca, Proca.Supporter, fpr_seed: System.get_env("FINGERPRINT_SEED", "")
 config :proca, Proca.Server.MTTWorker,
   max_messages_per_cycle: String.to_integer(System.get_env("MAX_MESSAGES_PER_CYCLE", "99"))
 
+config :proca, ProcaWeb.Telemetry,
+  enable: System.get_env("ENABLE_TELEMETRY", "true") == "true",
+  port: String.to_integer(System.get_env("METRICS_PORT", "9568"))
+
 # Configures Elixir's Logger
 config :logger,
   backends: [:console, {LoggerFileBackend, :error_log}, {LoggerFileBackend, :audit_log}],
