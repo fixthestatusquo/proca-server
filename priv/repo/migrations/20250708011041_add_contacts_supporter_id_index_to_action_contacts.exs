8b27428135ef81914dd0807d7cbc9b2320559614
anatolii <anatoliipolishchuk@gmail.com>
2025-07-08 04:35:24 +0300

    add index to the field supporter_id on table contacts

--- priv/repo/migrations/20250708011041_add_contacts_supporter_id_index_to_action_contacts.exs
+++ priv/repo/migrations/20250708011041_add_contacts_supporter_id_index_to_action_contacts.exs
@@ -2,6 +2,7 @@ defmodule Proca.Repo.Migrations.AddContactsSupporterIdIndexToActionContacts do
   use Ecto.Migration
 
   def change do
-
+    create_if_not_exists index(:contacts, [:supporter_id], name: :contacts_supporter_id_index)
   end
 end
+
