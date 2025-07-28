81a6c431b60f8c2b65dc7d1897d075ee0de17749
anatolii <anatoliipolishchuk@gmail.com>
2025-07-28 16:37:29 +0300

    add indexes to reduce query time of the counter, fix issue #311

---
+++ priv/repo/migrations/20250728132318_add_partial_processing_status_index_to_actions.exs
@@ -0,0 +1,11 @@
+defmodule Proca.Repo.Migrations.AddPartialProcessingStatusIndexToActions do
+  use Ecto.Migration
+
+  def change do
+    create_if_not_exists index(:actions,
+      [:supporter_id, :action_page_id, :campaign_id],
+      where: "processing_status IN (3, 4)",
+      name: :actions_partial_processing_status_index
+    )
+  end
+end
