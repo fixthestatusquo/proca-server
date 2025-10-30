722c0e202bdefa150a0c95d4d77a75be7d371162
1v4n4 <ivananl@yahoo.com>
2025-10-30 15:28:05 +0100

    Update absinthe

--- mix.exs
+++ mix.exs
@@ -63,7 +63,7 @@ defmodule Proca.MixProject do
       {:jason, "~> 1.2"},
       {:sentry, "~> 8.0"},
       {:plug_cowboy, "~> 2.3"},
-      {:absinthe, "1.7.0"},
+      {:absinthe, "~> 1.7.10"},
       {:absinthe_phoenix, "~> 2.0.2"},
       {:absinthe_plug, "~> 1.5.8"},
       {:cors_plug, "~> 2.0"},
