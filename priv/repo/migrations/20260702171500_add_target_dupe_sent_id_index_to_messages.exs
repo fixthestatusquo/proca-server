26b81d5bc7ab412ab266a4aab620d451e3a42e34
OliFady <oliver20191702008@cis.asu.edu.eg>
2026-07-02 20:27:02 +0300

    add message index

---
+++ priv/repo/migrations/20260702171500_add_target_dupe_sent_id_index_to_messages.exs
@@ -0,0 +1,12 @@
+defmodule Proca.Repo.Migrations.AddTargetDupeSentIdIndexToMessages do
+  use Ecto.Migration
+  @disable_ddl_transaction true
+  @disable_migration_lock true
+
+  def change do
+    create_if_not_exists index(:messages, [:target_id, :dupe_rank, :sent, :id],
+                           name: :messages_target_dupe_sent_id_index,
+                           concurrently: true
+                         )
+  end
+end
