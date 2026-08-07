8a208ceedd28320cf287640a73a4d5e65b06c0d3
1v4n4 <ivananl@yahoo.com>
2026-08-07 15:07:32 +0200

    Shorten Hackney pool timeout to prevent stale mailer connections closes #445

--- config/config.exs
+++ config/config.exs
@@ -151,6 +151,10 @@ config :proca, Proca.Server.MTTScheduler,
     23 => 0.02
   }
 
+config :hackney,
+  timeout: 42_000,
+  max_connections: 50
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
